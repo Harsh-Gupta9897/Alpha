f5455387279b6893ad8ac2b65490d45536a461ed
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2019-06-18 23:58:27 +0530

    Almost Done

---
+++ Mojo Snap/Assets/Datafiles/Scripts/InstructPanel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructPanel : MonoBehaviour
+{
+    public Animator anim;
+    void Update()
+    {    
+    }
+
+    public void setoff(Animator anim)
+    {
+        anim.SetBool("flag",false);
+    }
+
+    public void seton(Animator anim)
+    {
+        anim.SetBool("flag",true);
+    }
+}
