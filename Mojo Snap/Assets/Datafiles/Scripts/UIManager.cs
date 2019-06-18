7806b9cc07c19df1908c17a536c66fff33e435d4
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2019-06-18 10:50:17 +0530

    Almost Working

---
+++ Mojo Snap/Assets/Datafiles/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Recorder;
+public class UIManager : MonoBehaviour
+{
+    public RecordManager recordManager;
+    // Start is called before the first frame update
+    
+    public void StartVid()
+    {
+        recordManager.StartRecord();
+    }
+
+    public void SaveVid()
+    {
+        recordManager.StopRecord();
+    }
+
+
+}
---
+++ Mojo Snap/Assets/Datafiles/Scripts/comeback.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class comeback : MonoBehaviour
+{
+    public GameObject obj;
+
+        public void setoff()
+    {
+        obj.SetActive(false);
+        StartCoroutine(seton(0.1f, obj));
+    }
+
+
+    IEnumerator seton(float t1, GameObject obj)
+    {
+        yield return new WaitForSeconds(t1);
+        obj.SetActive(true);
+    }
+}
