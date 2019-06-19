cda88ce3881831facd0d302051d8e4ac1e6d3cf9
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2019-06-19 12:34:56 +0530

    Music Added

---
+++ Mojo Snap/Assets/Datafiles/Scripts/CamPer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class CamPer : MonoBehaviour
+{
+    void Awake()
+    {
+        if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            Permission.RequestUserPermission(Permission.Camera);
+        }
+    }
+}
